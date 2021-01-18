1fda48522c4a96a2e49ed1ee983656fa94dd7052
Ali_Taghipour <taghipourali19@gmail.com>
2021-01-19 03:20:34 +0330

    add build

---
+++ build/precache-manifest.db07ff915f77e7d930593b8bdb0bc441.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb871a97ac80fba811785d14047dbf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f134d5af24a6d8a76c3",
+    "url": "/static/css/2.07d820d4.chunk.css"
+  },
+  {
+    "revision": "937ba36ad6e815be67af",
+    "url": "/static/css/main.19f65f23.chunk.css"
+  },
+  {
+    "revision": "7f134d5af24a6d8a76c3",
+    "url": "/static/js/2.39109fe8.chunk.js"
+  },
+  {
+    "revision": "b2f8cd6a06d980838cb5a45128345104",
+    "url": "/static/js/2.39109fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "937ba36ad6e815be67af",
+    "url": "/static/js/main.d3fabfa1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "72722b668a91395b737dbc5321339122",
+    "url": "/static/media/Yekan.72722b66.ttf"
+  },
+  {
+    "revision": "8146981489938c20a78711c44dc7d5c5",
+    "url": "/static/media/back-logo.81469814.png"
+  },
+  {
+    "revision": "1da95bac1e0f6a112252b385696bc7fa",
+    "url": "/static/media/lines1.1da95bac.svg"
+  },
+  {
+    "revision": "0b8fe02731125943e30fda70f9ed2ba3",
+    "url": "/static/media/lines2.0b8fe027.svg"
+  },
+  {
+    "revision": "86c33bc14f0b6f4313cb0809034a92b9",
+    "url": "/static/media/question-mark-icon.86c33bc1.png"
+  }
+]);
